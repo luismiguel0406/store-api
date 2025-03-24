1485bbf5e1cfa2df1e697a1d2eeff3eb66bcedb4
Luis del orbe <luismigueldelorbe@hotmail.com>
2025-03-24 10:30:00 -0400

    haciendo migrations

---
+++ database/migrations/2025_03_24_140752_create_tbl_cliente_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tblCliente', function (Blueprint $table) {
+            $table->id('ClienteId');
+            $table->string('nombre');
+            $table->string('telefono');
+            $table->unsignedBigInteger('tipoClienteId');
+            $table->foreign('tipoClienteId')->references('TipoClienteId')->on('tblTipoCliente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tblCliente');
+    }
+};
